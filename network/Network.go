a59e88987c8a12f2fb896a0401a441cf4a733793
Pin <pinfake@hotmail.com>
2017-04-09 22:29:10 +0200

    Actually decode something maybe.

--- network/Network.go
+++ network/Network.go
@@ -9,14 +9,24 @@ type header struct {
 }
 
 type Message struct {
-	header [24]byte;
+	header []byte;
 	body []byte;
+}
+
+func decodeChunk(data []byte) ([] byte) {
+	decoded := []byte {};
+	i := 0;
+	for j := 0; j < 4; j++ {
+		decoded = append(decoded, data[i] ^ key[j]);
+		i++;
+	}
+	return decoded;
 }
 
 func Decode(data []byte) (Message, error) {
 	if len(data) < 24 {
 		return Message{}, errors.New("No header found");
 	}
-
-    return Message{}, nil;
+	header := decodeChunk(data[0:23]);
+    return Message{header,nil}, nil;
 }
