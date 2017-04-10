8fb92aa97f6cecc5e4cc90f0dbb99df589eab6fe
Pin <pinfake@hotmail.com>
2017-04-10 21:48:42 +0200

    I'm starting to like go :)

--- network/network.go
+++ network/network.go
@@ -4,6 +4,7 @@ import (
     "errors"
     "bytes"
     "encoding/binary"
+    "fmt"
 )
 
 const headerSize = 24;
@@ -11,10 +12,10 @@ const headerSize = 24;
 var key = []byte{0xa6, 0x77, 0x95, 0x7c}
 
 type Header struct {
-    query    uint16;
-    size     uint16;
-    sequence uint32;
-    unknown  [16]byte;
+    Query    uint16;
+    Size     uint16;
+    Sequence uint32;
+    Unknown  [16]byte;
 }
 
 type Message struct {
@@ -41,7 +42,7 @@ func Read(data []byte) (Message, error) {
     if len(data) < headerSize {
         return Message{}, errors.New("No header found");
     }
-    decoded := Mutate(data[0:headerSize-1]);
+    decoded := Mutate(data[0:headerSize]);
     buf := bytes.NewBuffer(decoded);
     header := Header{};
     err := binary.Read(buf, binary.BigEndian, &header);
