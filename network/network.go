d32435ba13b33b79393d2dfcd0bc8f1f09e2f4b8
Pin <pinfake@hotmail.com>
2017-04-10 21:10:10 +0200

    Oh god, tabs?, spaces? what's my ide doing? :/

--- network/network.go
+++ network/network.go
@@ -8,40 +8,40 @@ import (
 
 const headerSize = 24;
 
-var key = []byte {0xa6, 0x77, 0x95, 0x7c}
+var key = []byte{0xa6, 0x77, 0x95, 0x7c}
 
 type Header struct {
-    query uint16;
-    size uint16;
+    query    uint16;
+    size     uint16;
     sequence uint32;
-    unknown [16]byte;
+    unknown  [16]byte;
 }
 
 type Message struct {
-	header Header;
-	body []byte;
+    header Header;
+    body   []byte;
 }
 
 func Mutate(data []byte) ([] byte) {
-	decoded := []byte {};
-	i := 0;
+    decoded := []byte{};
+    i := 0;
     j := 0;
     for i < len(data) {
         decoded = append(decoded, data[i]^key[j]);
         j++;
         i++;
-        if j % 4 == 0 {
+        if j%4 == 0 {
             j = 0;
         }
     }
-	return decoded;
+    return decoded;
 }
 
 func Read(data []byte) (Message, error) {
-	if len(data) < headerSize {
-		return Message{}, errors.New("No header found");
-	}
-	decoded := Mutate(data[0:headerSize - 1]);
+    if len(data) < headerSize {
+        return Message{}, errors.New("No header found");
+    }
+    decoded := Mutate(data[0:headerSize-1]);
     buf := bytes.NewBuffer(decoded);
     header := Header{};
     err := binary.Read(buf, binary.BigEndian, &header);
--- network/network_test.go
+++ network/network_test.go
@@ -4,10 +4,10 @@ import "testing"
 
 func TestRead(t *testing.T) {
     t.Run("Should return an error on short buffer", func(t *testing.T) {
-        b := [] byte {};
+        b := [] byte{};
         _, err := Read(b);
         if err == nil {
-            t.Error( "No error on short byte array");
+            t.Error("No error on short byte array");
         }
     });
     t.Run("Should return mutated header", func(t *testing.T) {
