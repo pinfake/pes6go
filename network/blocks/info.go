825d42ac7a21c6ab1639cf30f2058dd10e66c404
Pin <pinfake@hotmail.com>
2017-04-14 13:49:41 +0200

    Learning, learning... hopefully I'm not doing things extremely bad...

--- network/blocks/info.go
+++ network/blocks/info.go
@@ -9,7 +9,6 @@ import (
 const dtLayout = "2006-01-02 15:04:05"
 
 type Info struct {
-    Body;
     time time.Time;
     title string;
     text string;
--- network/messages/discovery/init.go
+++ network/messages/discovery/init.go
@@ -1,13 +1,23 @@
 package discovery;
 
 import "github.com/pinfake/pes6go/network/blocks"
-import "github.com/pinfake/pes6go/network/messages"
+import (
+    "github.com/pinfake/pes6go/network/messages"
+    "time"
+)
 type Response struct {
+    title string;
+    time time.Time;
+    text string;
     messages.Message
 }
 
-func (Response) getBlocks() []blocks.Block {
+func (r Response) getBlocks() []blocks.Block {
     return []blocks.Block{
         blocks.NewBlock(0x0001, blocks.Zero{}),
+        blocks.NewBlock(0x0002, blocks.Info{
+            r.time, r.title, r.text,
+        }),
+        blocks.NewBlock(0x0003, blocks.Zero{}),
     }
 }
