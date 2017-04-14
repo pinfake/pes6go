3559a6869786887ec43396e478bfce248a54dbd3
Pin <pinfake@hotmail.com>
2017-04-14 13:27:51 +0200

    Hmm, this doesn't look so bad at all...

---
+++ network/blocks/info.go
@@ -0,0 +1,39 @@
+package blocks;
+
+import (
+    "encoding/binary"
+    "time"
+    "bytes"
+)
+
+const dtLayout = "2006-01-02 15:04:05"
+
+type Info struct {
+    Body;
+    time time.Time;
+    title string;
+    text string;
+}
+
+type internal struct {
+    header [6]byte;
+    time   [19]byte;
+    title  [64]byte;
+    text   [128]byte;
+}
+
+func buildInternal(info Info) internal {
+    return internal{
+        header: [6]byte{0x00, 0x00, 0x03, 0x10, 0x01, 0x00},
+        time:   [19]byte(info.time.Format(dtLayout)),
+        title:  [64]byte(info.title),
+        text:   [128]byte(info.text),
+    }
+
+}
+
+func (info Info) getData() []byte {
+    buf := bytes.Buffer{};
+    binary.Write(buf, binary.LittleEndian, buildInternal(info));
+    return buf.Bytes();
+}
