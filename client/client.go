f7dd48b46d26e64ebcab12652c1ab66507b57a8d
pin <pinfake@hotmail.com>
2017-07-21 11:10:53 +0200

    Trying to test the server using a custom client

---
+++ client/client.go
@@ -0,0 +1,16 @@
+package client
+
+import (
+	"net"
+	"strconv"
+)
+
+type Client struct {
+	conn net.Conn
+}
+
+func (c Client) Connect(host string, port int) error {
+	conn, err := net.Dial("tcp", host+":"+strconv.Itoa(port))
+	c.conn = conn
+	return err
+}
