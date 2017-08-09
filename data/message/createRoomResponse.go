987638df40b73a69bb48894fd8d177b70d57c54b
pin <pinfake@hotmail.com>
2017-08-09 19:15:32 +0200

    rooms, links wip... this is tough

--- data/message/createRoomResponse.go
+++ data/message/createRoomResponse.go
@@ -1,31 +1,28 @@
 package message
 
-//import (
-//	"reflect"
-//
-//	"github.com/pinfake/pes6go/data/block"
-//)
-//
-//type CreateRoomResponse struct {
-//	IpInfo []block.Piece
-//}
-//
-//func (r CreateRoomResponse) GetBlocks() []*block.Block {
-//	var blocks []*block.Block
-//
-//	blocks = append(blocks, block.GetBlocks(0x3087,
-//		[]block.Piece{block.PlayerSettingsHeader{r.PlayerId}})...,
-//	)
-//	blocks = append(blocks, block.GetBlocks(0x3088, r.PlayerSettings)...)
-//	blocks = append(blocks, block.GetBlocks(0x3089, []block.Piece{
-//		block.Uint32{0},
-//	})...)
-//	return blocks
-//}
-//
-//func NewPlayerSettingsMessage(playerId uint32, playerSettings block.PlayerSettings) PlayerSettings {
-//	return PlayerSettings{
-//		PlayerId:       playerId,
-//		PlayerSettings: block.GetPieces(reflect.ValueOf(playerSettings)),
-//	}
-//}
+import (
+	"reflect"
+
+	"github.com/pinfake/pes6go/data/block"
+)
+
+type CreateRoomResponse struct {
+	RoomLinks []block.Piece
+}
+
+func (r CreateRoomResponse) GetBlocks() []*block.Block {
+	var blocks []*block.Block
+
+	blocks = append(blocks, block.GetBlocks(0x4311, []block.Piece{block.Uint32{0}})...)
+	blocks = append(blocks, block.GetBlocks(0x4346, []block.Piece{block.Uint32{0}})...)
+	blocks = append(blocks, block.GetBlocks(0x4347, r.RoomLinks)...)
+	blocks = append(blocks, block.GetBlocks(0x4348, []block.Piece{block.Uint32{0}})...)
+	return blocks
+}
+
+func NewCreateRoomResponse(playerId uint32, playerSettings block.PlayerSettings) PlayerSettings {
+	return PlayerSettings{
+		PlayerId:       playerId,
+		PlayerSettings: block.GetPieces(reflect.ValueOf(playerSettings)),
+	}
+}
